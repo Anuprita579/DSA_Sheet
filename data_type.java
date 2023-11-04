ca1e41934e1ebfb3deeadf2f518e99f46fe0e177
Anuprita <anupritamhapankar22@gmail.com>
2023-11-04 16:52:24 +0530

    updated commit

---
+++ data_type.java
@@ -0,0 +1,16 @@
+public class data_type{
+    public static int dataTypes(String type) {
+        if(type.equals("Integer") || type.equals("Float")){
+            return 4;
+        }
+        else if(type.equals("Long") || type.equals("Double")){
+            return 8;
+        }
+        else if(type.equals("Character")){
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+}
