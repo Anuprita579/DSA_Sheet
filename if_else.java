83e840c2e0653fee185902167f098a49e6f34a6b
Anuprita <anupritamhapankar22@gmail.com>
2023-11-04 17:15:27 +0530

    updated commit

---
+++ if_else.java
@@ -0,0 +1,13 @@
+public class if_else{
+    public static String compareIfElse(int a, int b) {
+        if(a>b){
+            return "greater";
+        }
+        else if(a<b){
+            return "smaller";
+        }
+        else{
+            return "equal";
+        }
+    }
+}
