6fdcea66672fd89eac517e74996220b79db90f36
Anuprita <anupritamhapankar22@gmail.com>
2023-11-07 22:29:37 +0530

    updated commit

---
+++ pattern1.java
@@ -0,0 +1,10 @@
+public class pattern1 {
+    public static void nForest(int n) {
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
