4f137d5b060f699f15f1085a525f26e1a1bb4d57
Anuprita <anupritamhapankar22@gmail.com>
2023-11-07 22:37:21 +0530

    updated commit

--- pattern3.java
+++ pattern3.java
@@ -1,9 +1,9 @@
 public class pattern3 {
 
     public static void nForest(int n) {
-        for(int i=0; i<n; i++){
-            for(int j=0; j<=i; j++){
-                System.out.print("* ");
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+ " ");
             }
             System.out.print("\n");
         }
